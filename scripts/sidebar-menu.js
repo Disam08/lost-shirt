2e4f3bda08b2e781c5368c5e0a56070155768ec1
Dima Samusenko <108145097+Disam08@users.noreply.github.com>
2022-07-21 18:00:09 +0300

    work

--- scripts/sidebar-menu.js
+++ scripts/sidebar-menu.js
@@ -1,14 +1,15 @@
-(function(){
+(function () {
     const openingBtn = document.querySelector(".sidebar__hamburger");
-    const closingBtn = document.querySelector(".sidebar__hamburger");
     const sidebar = document.querySelector('.sidebar');
-    
-    
-    openingBtn.addEventListener("click", function(){
-        sidebar.classList.add("sidebar--opened");
+
+
+    openingBtn.addEventListener("click", function () {
+       const isOpen = sidebar.classList.length > 1
+        if (isOpen){
+            sidebar.classList.remove("sidebar__opened");
+            return
+        }
+        sidebar.classList.add("sidebar__opened");
     });
-    
-    closingBtn.addEventListener("click", function() {
-        sidebar.classList.remove("sidebar--opened");
-        });
+
 }());
